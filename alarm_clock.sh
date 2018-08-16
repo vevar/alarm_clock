00eb9d92dc247081288fd1fac24f4c56091ff80a
alxminyaev <alxminyaev@yandex.ru>
2018-08-16 10:53:36 +0700

    main

--- alarm_clock.sh
+++ alarm_clock.sh
@@ -1,72 +1,93 @@
 #!/bin/bash
 
-volume=20
-
-
+#location of script
 DIR="$(cd "$(dirname "$0")" && pwd)"
-me=`basename "$0"`
 
-scriptFile="$DIR/$me"
+#default sound of alarm
+tmpMusic=sounds/Cool-alarm-tone-notification-sound.mp3
 
-tmpMusic=/home/alxminyaev/Projects/eltex_course/labs_linux_networks/task3_Alarm/sounds/Cool-alarm-tone-notification-sound.mp3
-
-music=$2
-
-if [ -z $music ]
-    then
-    music=$tmpMusic
-fi
-
-cronFile=cron.txt
-
-param=$1
-
-
-off_alarms(){
+#Turn off alarms
+offAlarms(){
     jbs=( `ps axf | grep mplayer | grep -v grep | awk '{print $1}'` )
 	for job in ${jbs[*]} ; do
 		kill -15 $jbs
 	done
 }
 
-set_time(){
+#Turn on alarm
+onAlarm(){
+    
+    #default value of sound
+    local volume=20
+    #Set music of alarm
+    local music=$1
+
+    if [ -z "$music" ]
+    then
+        music="$DIR/$tmpMusic"
+    fi
+    
+    amixer -c 1 sset Master $volume% &> /dev/null &
+    mplayer $music -loop 0 &> /dev/null &
+}
+
+#New alarm clock
+addNewAlarm(){
     echo "Input time: "
+    
     echo "Hours:"
+    local hours
     read hours
         if [[ "$hours" != [0-9] ]] && [[ "$hours" != [0-2][0-9] ]] ; then
             echo 'Input right time".' >&2
             exit 10
         fi
+        
     echo "Minutes"
+    local minutes
     read minutes
         if [[ "$minutes" != [0-9] ]] && [[ "$minutes" != [0-9][0-9] ]] ; then
             echo 'Input right time".' >&2
             exit 10
         fi
-    echo $tm
+        
     echo "Input path to file: "
+    local music
     read music
+
     
-    
+    local me=`basename "$0"`
+    local scriptFile="$DIR/$me"
+    #Name of crontab file
+    local cronFile=crontab_rec_file
+
+    crontab -l > $cronFile
+    echo "$minutes $hours * * * $scriptFile -on $music" >> $cronFile
 }
 
-case $param in
-    "")
-        set_time
-        echo "$minutes $hours * * * $scriptFile -on $music" > $cronFile
-        crontab $cronFile
-        #sudo rtcwake -m mem -t $date
-        ;;
-    "-off")
-        off_alarms
-        ;;
-    "-on")
-        amixer -c 1 sset Master $volume% &> /dev/null &
-        mplayer $music -loop 0 &> /dev/null &
-        ;;    
-    *)
-        ;;
-esac
-
+for (( arg=1; arg<=$#; arg++))
+do
+    rec=${!arg:0:1}
+    if [ "$rec" = "-" ]
+    then
+        case ${!arg} in
+            #New alarm clock
+            "-n")
+                addNewAlarm
+                ;;
+            #Turn off alarms
+            "-off")
+                offAlarms
+                ;;
+            #Turn on alarm
+            "-on")
+                param=$(( arg + 1 ))
+                onAlarm ${!param}
+                ;;
+            *)
+                ;;
+        esac
+    fi
+done
 
 
